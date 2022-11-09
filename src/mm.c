4508d3e00099f32944328f1cf57e739ca3462be4
NojoudAlshehri <nojoud.alsh@gmail.com>
2022-11-09 22:14:12 +0900

    implement malloc(), free()

--- src/mm.c
+++ src/mm.c
@@ -58,6 +58,90 @@ static inline __attribute__((unused)) int block_index(size_t x)
         return 32 - __builtin_clz((unsigned int)x + 7);
     }
 }
+/* When requesting memory from the OS using sbrk(), request it in
+ * increments of CHUNK_SIZE. */
+#define CHUNK_SIZE (1 << 12)
+#define DSIZE 8
+
+#define PACK(size, alloc) ((size) | (alloc))
+
+#define GET(p) (*(p))
+#define PUT(p, val) (*(p) = (val))
+
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+#define NEXT_HEAD(p) ((Header *)((char *)p + GET_SIZE(p)))
+#define PREV_FREE_HEAD(p) (*(HEADER **)((char *)p + DSIZE))
+#define NEXT_FREE_HEAD(p) (*(HEADER **)((char *)p + 2 * DSIZE))
+
+#define HDRP(bp) ((Header *)((char *)(bp)-DSIZE))
+#define BLKP(p) ((char *)p + DSIZE)
+/*
+ * This function, defined in bulk.c, allocates a contiguous memory
+ * region of at least size bytes.  It MAY NOT BE USED as the allocator
+ * for pool-allocated regions.  Memory allocated using bulk_alloc()
+ * must be freed by bulk_free().
+ *
+ * This function will return NULL on failure.
+ */
+extern void *bulk_alloc(size_t size);
+
+/*
+ * This function is also defined in bulk.c, and it frees an allocation
+ * created with bulk_alloc().  Note that the pointer passed to this
+ * function MUST have been returned by bulk_alloc(), and the size MUST
+ * be the same as the size passed to bulk_alloc() when that memory was
+ * allocated.  Any other usage is likely to fail, and may crash your
+ * program.
+ */
+extern void bulk_free(void *ptr, size_t size);
+
+/*
+ * This function computes the log base 2 of the allocation block size
+ * for a given allocation.  To find the allocation block size from the
+ * result of this function, use 1 << block_size(x).
+ *
+ * Note that its results are NOT meaningful for any
+ * size > 4088!
+ *
+ * You do NOT need to understand how this function works.  If you are
+ * curious, see the gcc info page and search for __builtin_clz; it
+ * basically counts the number of leading binary zeroes in the value
+ * passed as its argument.
+ */
+static inline __attribute__((unused)) int block_index(size_t x)
+{
+    if (x <= 8)
+    {
+        return 5;
+    }
+    else
+    {
+        return 32 - __builtin_clz((unsigned int)x + 7);
+    }
+}
+
+typedef size_t Header;
+
+struct ExplicitMeta
+{
+    Header *pred;
+    Header *succ;
+};
+
+typedef struct ExplicitMeta explicitMeta;
+
+// static Header *freep = NULL;
+static explicitMeta *explicit_free_lists = NULL;
+
+extern void *sbrk(int increments);
+
+int block_index(size_t x);
+int init(void);
+static void *find_free_block(size_t asize);
+static Header *extend_heap();
+static void place(Header *hp, size_t asize);
 
 /*
  * You must implement malloc().  Your implementation of malloc() must be
@@ -65,17 +149,146 @@ static inline __attribute__((unused)) int block_index(size_t x)
  */
 void *malloc(size_t size)
 {
-    size_t asize;
-    size_t extendsize;
-    char *bp;
-
     if (size == 0)
         return NULL;
+    if (size > CHUNK_SIZE - DSIZE)
+    {
+        Header *hp = (Header *)bulk_alloc(size + DSIZE);
+        PUT(hp, PACK(size, 1));
+        return BLKP(hp);
+    }
+    size_t asize = 1 << block_index(size);
+    Header *hp;
+    if ((hp = find_free_block(asize)) != NULL)
+    {
+        place(hp, asize);
+        return BLKP(hp);
+    }
 
-    if (size <= CHUNK_SIZE)
-        asize = 2 * CHUNK_SIZE;
+    if ((hp = extend_heap()) == NULL)
+        return NULL;
+    place(hp, asize);
+    return BLKP(hp);
+}
+
+static void place(Header *hp, size_t asize)
+{
+    size_t size = GET_SIZE(hp);
+    explicitMeta *exMeta = (explicitMeta *)BLKP(hp);
+    Header *pred = exMeta->pred;
+    Header *succ = exMeta->succ;
+    explicitMeta *predExMeta = (explicitMeta *)BLKP(*pred);
+    explicitMeta *succExMeta = (explicitMeta *)BLKP(*succ);
+    if (size == asize)
+    {
+        PUT(hp, PACK(asize, 1));
+        if (pred != hp && succ != hp)
+        {
+            predExMeta->succ = succ;
+            succExMeta->pred = pred;
+        }
+        else if (pred != hp && succ == hp)
+        {
+            predExMeta->succ = pred;
+            explicit_free_lists = predExMeta;
+        }
+        else if (pred == hp && succ != hp)
+        {
+            succExMeta->pred = succ;
+        }
+        else
+        {
+            explicit_free_lists = NULL;
+        }
+    }
     else
-        asize = CHUNK_SIZE * ((size + (CHUNK_SIZE) + (CHUNK_SIZE - 1)) / CHUNK_SIZE) return bulk_alloc(size);
+    {
+        PUT(hp, PACK(asize, 1));
+        Header *next_hp = NEXT_HEAD(hp);
+        size -= asize;
+        PUT(next_hp, PACK(size, 0));
+        explicitMeta *nextExMeta = (explicitMeta *)BLKP(next_hp);
+        if (pred != hp && succ != hp)
+        {
+            nextExMeta->pred = pred;
+            nextExMeta->succ = succ;
+            predExMeta->succ = next_hp;
+            succExMeta->pred = next_hp;
+        }
+        else if (pred != hp && succ == hp)
+        {
+            predExMeta->succ = next_hp;
+            nextExMeta->pred = pred;
+            nextExMeta->succ = next_hp;
+            explicit_free_lists = nextExMeta;
+        }
+        else if (pred == hp && succ != hp)
+        {
+            nextExMeta->pred = next_hp;
+            nextExMeta->succ = succ;
+            succExMeta->pred = next_hp;
+        }
+        else
+        {
+            nextExMeta->pred = next_hp;
+            nextExMeta->succ = next_hp;
+            explicit_free_lists = nextExMeta;
+        }
+    }
+}
+
+static void *find_free_block(size_t asize)
+{
+    Header *p, *prevp;
+    if (explicit_free_lists == NULL)
+    {
+        return extend_heap();
+    }
+    p = explicit_free_lists->succ;
+    prevp = explicit_free_lists->pred;
+    while (prevp != p)
+    {
+        if (GET_SIZE(p) >= asize)
+        {
+            return p;
+        }
+        p = prevp;
+        explicitMeta *exMeta = (explicitMeta *)BLKP(p);
+        prevp = exMeta->pred;
+    }
+    if (GET_SIZE(p) >= asize)
+    {
+        return p;
+    }
+    return NULL;
+}
+
+static Header *extend_heap()
+{
+    void *p = sbrk(CHUNK_SIZE);
+
+    if (p == (void *)-1)
+    {
+        return NULL;
+    }
+
+    Header *hp = (Header *)p;
+    PUT(hp, PACK(CHUNK_SIZE, 0));
+    explicitMeta *exMeta = (explicitMeta *)BLKP(hp);
+    if (explicit_free_lists == NULL)
+    { // there's no free block
+        exMeta->pred = hp;
+        exMeta->succ = hp;
+        explicit_free_lists = exMeta;
+    }
+    else
+    {
+        exMeta->pred = explicit_free_lists->succ;
+        explicit_free_lists->succ = hp;
+        exMeta->succ = hp;
+        explicit_free_lists = exMeta;
+    }
+    return hp;
 }
 
 /*
@@ -91,8 +304,9 @@ void *malloc(size_t size)
  */
 void *calloc(size_t nmemb, size_t size)
 {
-    void *ptr = bulk_alloc(nmemb * size);
-    memset(ptr, 0, nmemb * size);
+    size_t total_size = nmemb * size;
+    void *ptr = malloc(total_size);
+    memset(ptr, 0, total_size);
     return ptr;
 }
 
@@ -110,8 +324,14 @@ void *calloc(size_t nmemb, size_t size)
  */
 void *realloc(void *ptr, size_t size)
 {
-    fprintf(stderr, "Realloc is not implemented!\n");
-    return NULL;
+    Header hp = HDRP(ptr);
+    size_t block_size = GET_SIZE(hp);
+    if (block_size > size)
+        return ptr;
+    void *new_ptr = malloc(size);
+    memcpy(new_ptr, ptr, block_size - DSIZE);
+    free(ptr);
+    return new_ptr;
 }
 
 /*
@@ -123,5 +343,18 @@ void *realloc(void *ptr, size_t size)
  */
 void free(void *ptr)
 {
+    Header *hp = HDRP(ptr);
+    size_t size = GET_SIZE(hp);
+    if (size > CHUNK_SIZE - DSIZE)
+    {
+        bulk_free(hp, size);
+    }
+    PUT(hp, PACK(size, 0));
+    explicitMeta *exMeta = (explicitMeta *)BLKP(hp);
+    Header *last_free = explicit_free_lists->succ;
+    explicit_free_lists->succ = hp;
+    exMeta->pred = last_free;
+    exMeta->succ = hp;
+    explicit_free_lists = exMeta;
     return;
 }
